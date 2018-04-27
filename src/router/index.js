22d7807557f077f2784d3fe4307f39abba84a99e
fengjinlong <18618166564@163.com>
2018-04-27 18:31:29 +0800

    adda

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import login from '@/components/login'
-import user from '@/components/user'
 
 const _import = require('./_import_' + process.env.NODE_ENV)
 
